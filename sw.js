4cd021ecbaffb7f8782305041314af73bc3287ab
isaelalbuquerque-stack <isael.albuquerque@gmail.com>
2026-09-08 00:27:30 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,5 @@
-const CACHE='jpmicangas-designer-v4';
+const CACHE='jpmicangas-designer-v5';
 const ASSETS=['./','index.html','styles.css','app.js','manifest.webmanifest','icons/icon-192.png','icons/icon-512.png','icons/logo-jpmicangas.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
-  const copy=resp.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return resp;
-}).catch(()=>caches.match('./')))));
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match('./'))))});
